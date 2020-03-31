6df9da11fe70abfb311e70b954d5d49841202c47
Unknown <michaelkovaliov97@gmail.com>
2020-03-31 18:39:59 +0300

    Added tests for ts_generator

---
+++ test_ts.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pytest
+
+from ts_generator import generate_ar_k, is_stationary_ar_k
+
+
+def test_generate():
+    alphas = np.array([0.5, 0.4])
+    generated_ar = generate_ar_k(alphas, 1000, np.random.normal)
+    print(generated_ar)
+
+
+def test_stationary():
+    assert is_stationary_ar_k([0.5, 0.4])
+    assert not is_stationary_ar_k(np.array([0.5, 0.5]))
