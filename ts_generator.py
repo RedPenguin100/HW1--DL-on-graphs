cdfb0d6f3cfd8112248bc506a47ec19c10303d9e
Unknown <michaelkovaliov97@gmail.com>
2020-04-10 18:43:55 +0300

    Added utility function for prediction

--- ts_generator.py
+++ ts_generator.py
@@ -4,6 +4,8 @@ import numpy as np
 def get_last_n(arr: list, n: int):
     return arr[-n:]
 
+def get_best_prediction(last_k_values, alphas):
+    return alphas.dot(list(reversed(last_k_values)))
 
 def is_stationary_ar_k(alphas: np.array):
     """
@@ -34,7 +36,7 @@ def generate_ar_k(alphas: np.array, N: int, noise_func) -> np.array:
     ar_k_series = list(init_condition)
     while len(ar_k_series) < N:
         # The first alpha is multiplied by the last x, and so on, so we reverse the Xs
-        new_value = alphas.dot(list(reversed(get_last_n(ar_k_series, k)))) + noise_func()
+        new_value = get_best_prediction(get_last_n(ar_k_series, k), alphas) + noise_func()
         ar_k_series.append(new_value)
 
     return np.array(ar_k_series)
